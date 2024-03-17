3229fbd00a0c6d35923727ea18bf96876b2fe248
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2024-03-17 10:00:55 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -46,7 +46,7 @@ async def get_stats(bot, message):
 async def donate(client, message):
 	text = "<b>🥲 Thanks For Showing Interest In Donation! ❤️</b> \n\nIf You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice. \n\n<b>🛍 UPI ID:</b> <code>suhailebrahim99@okicici</code>"
 	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/CallAdminRobot"), 
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/Betterproffesor"), 
         			InlineKeyboardButton("✖️ Close",callback_data = "close_btn") ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
